e9ecb15cea531fc87cfc263699c73a80d061d4ef
Pablo Morales <pmorales.contacto@gmail.com>
2020-10-18 01:57:09 -0300

    canvas añadido con uso de forEach

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,44 @@
 $(document).ready(function(){
-    pokemon(25)
-    
+    pokemon(25)    
 })
 
-
+// función llamada pokemon
 let pokemon = (eleccionUsuario) =>{          
     $.ajax({
         type: "GET",
         url: `https://pokeapi.co/api/v2/pokemon/${eleccionUsuario}`,
         datatype: "json",
-        success: function(datosPokemon){
-            
+        success: function(datosPokemon){            
             $("#sprite").empty()
             $("#sprite").append(`<img src=${datosPokemon.sprites.front_default}>`)
+            //arreglo que almacenará hp, atk, def, etc.
+            var data = []
+            // recorrer datosPokemon.stats
+            datosPokemon.stats.forEach(function(e){
+                data.push({y: e.base_stat, label:e.base_stat})
+            })            
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                theme: "light2",
+                title: {
+                    text: "Características Pokemón"
+                },
+                axisY: {
+                    title: "Units",
+                    titleFontSize: 24,
+                    includeZero: true
+                },
+                data: [{
+                    type: "column",
+                    yValueFormatString: "#,### Units",
+                    dataPoints: data                        
+                }]
+            })
+            chart.render();
             },            
         })             
     }
-    
+// Botón para iniciar llamada pokemon    
 $("#btn").click(()=>{
     let endPointUser = $("input").val()
     let patronBusquedaNumero = /^[\d]+$/gim    
@@ -26,8 +48,12 @@ $("#btn").click(()=>{
         alert('ingrese los valores nuevamente')
     } else {
 
-        pokemon(endPointUser)
+        pokemon(endPointUser)        
     }
 })
+
+
+
+
 
 
